713ef6ed18c0fdfd591abacdf29e6107341384cd
mysterybee07 <biraj2019pudasaini@gmail.com>
2024-11-23 17:49:05 +0545

    database connection done

--- go-job/cmd/main/main.go
+++ go-job/cmd/main/main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/mysterybee07/go-react-job/database"
+	"github.com/mysterybee07/go-react-job/utils"
+)
+
+func init() {
+	utils.LoadEnv()
+	database.ConnectDB()
+}
+
+func main() {
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	r := gin.Default()
+
+	// Example route
+	r.GET("", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	// Start the server
+	err := r.Run(":" + port) // listen and serve on the specified port
+	if err != nil {
+		panic("Failed to start server: " + err.Error())
+	}
+}
---
+++ go-job/database/connect.go
@@ -0,0 +1,48 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/mysterybee07/go-react-job/utils"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	var err error
+	utils.LoadEnv()
+
+	// Read environment variables
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+
+	// Validate required environment variables
+	if dbUser == "" || dbHost == "" || dbName == "" {
+		log.Fatal("One or more required environment variables (DB_USER, DB_HOST, DB_NAME) are not set")
+	}
+
+	// Handle case where password is not set (leave it empty)
+	if dbPassword == "" {
+		dbPassword = ""
+	}
+
+	// Build the DSN (Data Source Name) dynamically
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		dbUser, dbPassword, dbHost, dbName)
+
+	// Open a connection to the database using GORM
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+	}
+
+	// Log successful connection
+	log.Println("Connected to database successfully")
+
+}
